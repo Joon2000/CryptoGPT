0cd0165f236e2093a7cb14cdf1663765e8f9cc1c
Joon2000 <103424415+Joon2000@users.noreply.github.com>
2023-10-24 02:35:26 +0900

    Fix getData to return walletData

--- app/Data/wagmiData.ts
+++ app/Data/wagmiData.ts
@@ -1,18 +1,17 @@
+import { Address, FetchBalanceResult, GetNetworkResult } from "@wagmi/core";
 import { fetchBalanceData, getNetworkData } from "./../utils/wagmiFunction";
-import { useAccount } from "wagmi";
 
-export const getData = () => {
-  const account = useAccount({
-    async onConnect() {
-      const balance = await fetchBalanceData(account.address!);
-      const network = getNetworkData();
-      const walletData = {
-        walletUserData: {
-          address: account.address,
-          balance: balance,
-          network: network,
-        },
-      };
-    },
-  });
+export const getData = async (address: Address | undefined) => {
+  try {
+    const balance = await fetchBalanceData(address!);
+    const formattedBalance = balance.formatted;
+    const network = getNetworkData();
+    const walletData = {
+      address: address,
+      balance: formattedBalance,
+      network: network,
+    };
+    console.log(walletData);
+    return walletData;
+  } catch (err) {}
 };
