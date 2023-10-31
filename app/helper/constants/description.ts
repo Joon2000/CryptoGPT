eb94316d5139c9c51c8b33eaf3716eb8f07b5e25
Joon2000 <103424415+Joon2000@users.noreply.github.com>
2023-11-01 03:25:34 +0900

    Add description

---
+++ app/helper/constants/description.ts
@@ -0,0 +1,5 @@
+export const fetchCryptoPriceDescription =
+  "Fetches the current price of a specified cryptocurrency";
+
+export const fetchWalletDataDescription =
+  "Fetches the Wallet Data of the user's blockchain wallet. Currently it contains user's blockchain wallet address, user's certain crypto balance, the blockchain network user is currently ussing";
