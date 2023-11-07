d3311692dcc239adf95b26cff46932b2364323f6
Joon2000 <103424415+Joon2000@users.noreply.github.com>
2023-11-07 20:01:52 +0900

    Fix css

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -11,14 +11,14 @@ export default function ChatBot() {
       <div className="flex justify-end mt-10 mr-6">
         <ConnectKitButton />
       </div>
-      <div className="GPT--container flex max-w-6xl mx-auto items-center justify-center p-24">
-        <div className="flex flex-col gap-12">
-          <h1 className="text-gray-200 font-extrabold text-6xl">
+      <div className="GPT--container flex max-w-6xl mx-auto justify-center pt-16 h-screen">
+        <div className="flex flex-col gap-12 w-3/5 ">
+          <h1 className="text-gray-200 font-extrabold text-6xl text-center">
             Crypto GPT🔗
           </h1>
-          <div className="flex flex-col h-80">
+          <div className="chat-message-input-container flex flex-col h-3/5">
             <ChatMessages className="px-2 py-3 flex-1" />
-            <ChatInput className="px-4" />
+            <ChatInput className="px-4 " />
           </div>
         </div>
       </div>
